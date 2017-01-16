8d25b6b8ba2c022bf12eb76af1e3a2aa90364ec8
Ranger <Ranger@xianyu.io>
2017-01-16 16:25:16 +0800

    add: assert doesNotThrow API

---
+++ Assert/assert-doesNotThrow.js
@@ -0,0 +1,12 @@
+/**
+ * 断言函数
+ */
+const assert = require('assert');
+
+/**
+ * assert.doesNotThrow(block[,error][,message]);
+ * 断言block函数不会抛出错误
+ * 暂时没看明白 留坑
+ * */
+
+// TODO assert.doesNotThrow(block[,error][,message])
