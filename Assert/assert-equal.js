4e0585187ccf5778f00cf05008bde938bf63ff60
Ranger <Ranger@xianyu.io>
2017-01-17 16:54:35 +0800

    complete: assert equal API

--- Assert/assert-equal.js
+++ Assert/assert-equal.js
@@ -18,3 +18,12 @@ const assert = require('assert');
 // assert.equal({a: {b: 1}},{a: {b: 1}});
 // 抛出 AssertionError: { a: { b: 1 } } == { a: { b: 1 } } 异常
 // 因为equal使用的是（==）操作符，此时比较的是两个字面量对象的内存地址，所以不通过
+
+// assert.equal({},{},'object not equal!');
+// 抛出 AssertionError: object not equal! 指定message信息异常
+
+/**
+ * 总结：由于equal使用的是相等（==）操作符
+ * 所以该API最好是只用于ES原始类型的断言
+ * 对象类型的断言请使用deepEqual或者deepStrictEqual
+ * */
