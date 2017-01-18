abf4624b47d36ebbb4baac3c79504d7e159dace7
Ranger <Ranger@xianyu.io>
2017-01-18 10:11:45 +0800

    add: assert notEqual API

---
+++ Assert/assert-notEqual.js
@@ -0,0 +1,20 @@
+/**
+ * 断言模块
+ */
+
+const assert = require('assert');
+
+/**
+ * assert.notEqual(actual,expected[,message])
+ * 使用不等运算符（!=）来测试actual和expected是否不相等
+ * 注意 message 参数是可选的
+ * */
+
+// assert.notEqual(1,2);
+// 通过
+
+// assert.notEqual(1,1);
+// 抛出 AssertionError: 1 != 1 异常
+
+// assert.notEqual(1,'1');
+// 抛出 AssertionError: 1 != '1' 异常
