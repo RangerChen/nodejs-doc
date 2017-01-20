203247b85d3232eeaff2fe154f3f8a5fb498f527
Ranger <Ranger@xianyu.io>
2017-01-20 11:26:58 +0800

    update: fileSystem readFile API

--- FileSystem/fs-readfile.js
+++ FileSystem/fs-readfile.js
@@ -4,3 +4,12 @@
 
 const fs = require('fs');
 
+/**
+ * readFile() 用于异步读取数据
+ * fs.readFile(path,callback)
+ * path为读取文件的路径
+ * */
+fs.readFile('./ori-file.fl',function (error, buffer) {
+   if(error) throw error;
+   console.log(buffer);
+});
