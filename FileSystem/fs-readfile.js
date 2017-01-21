6bb798f4e688ba9a2cdc8ba6eb8e0155083f1888
Ranger <Ranger@xianyu.io>
2017-01-21 17:26:32 +0800

    add: readFileSync API

--- FileSystem/fs-readfile.js
+++ FileSystem/fs-readfile.js
@@ -7,9 +7,17 @@ const fs = require('fs');
 /**
  * readFile() 用于异步读取数据
  * fs.readFile(path,callback)
- * path为读取文件的路径
+ * path为读取文件的路径，可以是相对路径，也可以是绝对路径
+ * callback为读取完成后的回调函数，回调函数的第一个参数为发生错误时的错误对象
  * */
 fs.readFile('./ori-file.fl',function (error, buffer) {
    if(error) throw error;
    console.log(buffer);
 });
+
+/**
+ * readFileSync()用于同步读取一个文件，返回一个字符串
+ * */
+
+var text = fs.readFileSync('./ori-file.fl','utf-8');
+console.log(text);
