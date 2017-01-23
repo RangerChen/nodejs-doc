a390341f73fc7b13fb66b86531982e84946450c6
Ranger <Ranger@xianyu.io>
2017-01-23 11:21:05 +0800

    update：FileSystem readFile API with security

--- FileSystem/fs-readfile.js
+++ FileSystem/fs-readfile.js
@@ -11,6 +11,7 @@ const fs = require('fs');
  * callback为读取完成后的回调函数，回调函数的第一个参数为发生错误时的错误对象
  * 注意下列代码中的buffer 默认输出的是缓存的二级制数据，如果需要看到明文内容，可以使用buffer的toString()方法
  * */
+
 fs.readFile('./ori-file.fl',function (error, buffer) {
    if(error) throw error;
    console.log(buffer.toString());
@@ -20,5 +21,13 @@ fs.readFile('./ori-file.fl',function (error, buffer) {
  * readFileSync()用于同步读取一个文件，返回一个字符串
  * */
 
-/*var text = fs.readFileSync('./ori-file.fl','utf-8');
-console.log(text);*/
+var text = fs.readFileSync('./ori-file.fl','utf-8');
+console.log(text);
+
+// ※※※ 由于readFile方法是一部操作，所以必须小心，不要同时发起多个readFile请求！
+// 下列代码能很快的将资源耗尽
+for(var i = 0 ; i < 1000 ; i ++){
+    fs.readFile('./' + i + '.txt',function (error) {
+        // do something operate
+    });
+}
