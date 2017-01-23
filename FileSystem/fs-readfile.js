8a6b1462fef5b538e2b3bcaf65ebe78530898318
Ranger <Ranger@xianyu.io>
2017-01-23 11:17:20 +0800

    update：FileSystem readFile API

--- FileSystem/fs-readfile.js
+++ FileSystem/fs-readfile.js
@@ -9,15 +9,16 @@ const fs = require('fs');
  * fs.readFile(path,callback)
  * path为读取文件的路径，可以是相对路径，也可以是绝对路径
  * callback为读取完成后的回调函数，回调函数的第一个参数为发生错误时的错误对象
+ * 注意下列代码中的buffer 默认输出的是缓存的二级制数据，如果需要看到明文内容，可以使用buffer的toString()方法
  * */
 fs.readFile('./ori-file.fl',function (error, buffer) {
    if(error) throw error;
-   console.log(buffer);
+   console.log(buffer.toString());
 });
 
 /**
  * readFileSync()用于同步读取一个文件，返回一个字符串
  * */
 
-var text = fs.readFileSync('./ori-file.fl','utf-8');
-console.log(text);
+/*var text = fs.readFileSync('./ori-file.fl','utf-8');
+console.log(text);*/
