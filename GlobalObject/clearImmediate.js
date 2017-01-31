65effae49adf734f1998724c297b81aeb48e3477
Ranger <Ranger@xianyu.io>
2017-01-31 15:04:07 +0800

    update：GlobalObject cleatImmediate documentation

--- GlobalObject/clearImmediate.js
+++ GlobalObject/clearImmediate.js
@@ -3,10 +3,11 @@
  * */
 
 /**
- * clearImmediate(setImmediateId)
+ * clearImmediate(Timeout)
  * 数据类型：函数 Function
  * 清楚一个立即运行函数的引用
  * 前提是，配合setImmediate来使用
+ * setImmediate()函数会返回一个Timeout对象（需要注意的是，所有的setXx返回的都是一个Timeout对象，而不是一个id）
  * */
 
 console.log(clearImmediate.toString());
