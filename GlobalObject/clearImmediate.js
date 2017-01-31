76155ec6a2ecd026917247907938eefed59cd330
Ranger <Ranger@xianyu.io>
2017-01-31 14:57:08 +0800

    update：GlobalObject cleatImmediate documentation

--- GlobalObject/clearImmediate.js
+++ GlobalObject/clearImmediate.js
@@ -4,9 +4,24 @@
 
 /**
  * clearImmediate(setImmediateId)
+ * 数据类型：函数 Function
  * 清楚一个立即运行函数的引用
  * 前提是，配合setImmediate来使用
  * */
 
 console.log(clearImmediate.toString());
 
+/**
+ * 对应源码
+ * */
+var newCleatImmediate = function (immediate) {
+    if(!immediate) return;
+
+    immediate._onImmediate = null;
+
+    immediateQueue.remove(immediate);
+
+    if(!immediateQueue.head){
+        process._needImmediateCallback = false;
+    }
+};
