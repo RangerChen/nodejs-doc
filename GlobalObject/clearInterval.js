9976c8390ce61a3fc302c27791b5ecb960e31167
Ranger <Ranger@xianyu.io>
2017-02-01 15:53:02 +0800

    update：GlobalObject cleatInterval documentation

---
+++ GlobalObject/clearInterval.js
@@ -0,0 +1,14 @@
+/**
+ * 全局对象
+ * */
+
+/**
+ *
+ * cleatInterval(Timeout)
+ * 数据类型：函数 Function
+ * Timeout有setTimeout函数返回
+ * */
+
+console.log(clearTimeout.toString());
+
+console.log(setTimeout(function () {},200));
