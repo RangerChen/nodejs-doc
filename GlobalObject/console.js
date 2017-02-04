2f4f534cfaf8df8671732852d031886d5d7e4517
Ranger <Ranger@xianyu.io>
2017-02-04 16:16:48 +0800

    add: GlobalObject console documentation

---
+++ GlobalObject/console.js
@@ -0,0 +1,21 @@
+/**
+ * 全局对象
+ * */
+
+/**
+ * console 控制台对象
+ * 主要用于标准输出
+ * 对象所拥有的方法如下：
+ * log()
+ * info()
+ * warn()
+ * error()
+ * dir()
+ * time()
+ * timeEnd()
+ * trace()
+ * assert()
+ * Console()
+ * */
+
+console.log(console);
