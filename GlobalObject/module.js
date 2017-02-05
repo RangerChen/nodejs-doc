c1b6f85600f2d53a7d60df0daabb5d51cf64f21e
Ranger <Ranger@xianyu.io>
2017-02-05 16:32:20 +0800

    add: GlobalObject module documentation

---
+++ GlobalObject/module.js
@@ -0,0 +1,23 @@
+/**
+ * 全局对象
+ * */
+
+/**
+ * 一个当前模块的引用
+ * */
+console.log(module);
+
+var Module = {
+    id: '.',
+    exports: {},
+    parent: null,
+    filename: '/Users/Ranger.Chen/front-side/nodejs_doc/GlobalObject/module.js',
+    loaded: false,
+    children: [],
+    paths: ['/Users/Ranger.Chen/front-side/nodejs_doc/GlobalObject/node_modules',
+        '/Users/Ranger.Chen/front-side/nodejs_doc/node_modules',
+        '/Users/Ranger.Chen/front-side/node_modules',
+        '/Users/Ranger.Chen/node_modules',
+        '/Users/node_modules',
+        '/node_modules']
+};
